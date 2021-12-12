5e3115874db0cf5703a5911a214dff7b5dd92dfe
Eric Baker <eric100596@aol.com>
2021-12-12 13:58:40 -0700

    Capstone

--- components/Nav.js
+++ components/Nav.js
@@ -1,12 +1,13 @@
-import html from "html-literal";
+import links from "../store/Links";
 
-export default () => html`
-  <nav style="position: relative; top: 60px" ;>
+export default links => `
+  <nav
+  <style="position: relative; top: 60px";>
     <i class="fas fa-bars"></i>
     <ul class="hidden--mobile">
-      <a href="Aboutthisgame.html"> About This Game</a>
-      <a href="Strategy.html"> Strategy</a>
-      <a href="Instructions.html"> Instructions </a>
+      ${Links.map(
+        link => `<li><a href="/${link.title}" data-navigo>${link.text}</a></li>`
+      ).join("")}
     </ul>
   </nav>
 `;
---
+++ store/Links.js
@@ -0,0 +1,7 @@
+export default [
+export { title: "Home", text: "Home" },
+export { title: "Instructions", text: "Instructions" },
+export { title: "About", text: "About this game" },
+export { title: "Strategy", text: "Strategy" },
+export { title: "Contact", text: "Contact"}
+]
