928eca6f8c6459282896d296ab67194520567a4e
Eric Baker <eric100596@aol.com>
2021-12-16 19:17:48 -0600

    Capstone

--- components/views/Home.js
+++ components/views/Home.js
@@ -1,7 +1,16 @@
 import html from "html-literal";
 
-export default () => html`
+const kelvinToFahrenheit = kelvinTemp =>
+  Math.round((kelvinTemp - 273.15) * (9 / 5) + 32);
+
+// eslint-disable-next-line prettier/prettier
+export default (st) => html`
   <section id="Home">
+    <h3>
+      Temperature in ${st.weather.city} is
+      ${kelvinToFahrenheit(st.weather.temp)}F. It feels like
+      ${kelvinToFahrenheit(st.weather.feelsLike)}F.
+    </h3>
     <div class="boxtop">
       <p>GAME SPACE</p>
     </div>
